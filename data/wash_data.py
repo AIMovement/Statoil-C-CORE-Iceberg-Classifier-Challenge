e1223b18dea214996394a1c32f8c911a9fbf51f6
Björn Andersson <andebjor@gmail.com>
2017-11-08 20:39:15 +0100

    Don't fail if test data is not available

--- data/wash_data.py
+++ data/wash_data.py
@@ -3,15 +3,20 @@
 import sys
 import os
 import json
-import lzma
 
 
 # If you want to re-run this you need to download the data sets from the kaggle
 # site, https://www.kaggle.com/c/statoil-iceberg-classifier-challenge/data ,
 # unpack, and place the .json files in this foler.
 def main():
+    # It's optional to process the test data
+    has_test = True
+
     train_set = load_json('train.json')
-    test_set  = load_json('test.json')
+    try:
+        test_set  = load_json('test.json')
+    except IOError as e:
+        has_test = False
 
     # print keys in the data sets
 #      print(train_set[0].keys())
@@ -37,16 +42,19 @@ def main():
                                  'inc_angle':  img['inc_angle'],
                                  'is_iceberg': img['is_iceberg']}))
 
-    os.makedirs('test_data')
-    for (k, img) in enumerate(test_set):
-        print('Test image {} of {}'.format(k, len(test_set)))
+    if has_test:
+        os.makedirs('test_data')
+        for (k, img) in enumerate(test_set):
+            print('Test image {} of {}'.format(k, len(test_set)))
 
-        fname = os.path.join('test_data', img['id'] + '.json')
+            fname = os.path.join('test_data', img['id'] + '.json')
 
-        with open(fname, 'w') as fp:
-            fp.write(json.dumps({'band_1':    img['band_1'],
-                                 'band_2':    img['band_2'],
-                                 'inc_angle': img['inc_angle']}))
+            with open(fname, 'w') as fp:
+                fp.write(json.dumps({'band_1':    img['band_1'],
+                                     'band_2':    img['band_2'],
+                                     'inc_angle': img['inc_angle']}))
+    else:
+        print('Test data not saved, only train data!')
 
     return True
 
